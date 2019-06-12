f38aab36107bf678da8e5e8e0b15360525abdbeb
zyxtaxbo <46652051+zyxtaxbo@users.noreply.github.com>
2019-06-12 18:02:13 +0200

    web-weiterleitung per script erstellt
    ansible-web.sh erstellt
    
    Benutzung: ./ansible-web.sh +host bzw. +hostvar/...
    
    Das Skript öffnet per python einen webbrowser zur Zielseite und erstellt eine Portweiterleitung.
    Am lokalen Port wird eine "1" am Anfang hinzugefügt um Kollisionen mit z.B. Port 443 zu vermeiden.
    
    Die Host_vars Dateien für die Server mussten um die Variablen "ansible_web_port" und "ansible_web_subdir" erweitert werden.

---
+++ ansible-web.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+
+ARG=$1
+
+if [ ${ARG:0:10} == "host_vars/" ] ; then
+        echo Found host_vars-file as argument
+        ARG=${ARG:10:${#ARG}}
+        ARG=${ARG:0:${#ARG} - 4}
+        echo revised argument: $ARG
+
+fi
+
+JQ=$(which jq)
+
+if [ -z "$ARG" ] ; then
+   echo 'please spezify a hostname' >&2
+   exit 1
+fi
+
+if [ -z "$JQ" ] ; then
+   echo 'jq not found, please install it with "apt install jq"' >&2
+   exit 2
+fi
+
+JSON=$(ansible-inventory  --host=${ARG})
+ANSIBLE_HOST=$(echo $JSON |$JQ -r .ansible_host )
+ANSIBLE_PORT=$(echo $JSON |$JQ -r '.ansible_port // 822' )
+ANSIBLE_USER=$(echo $JSON |$JQ -r '.ansible_user // "root"' )
+ANSIBLE_ARGS=$(echo $JSON |$JQ -r '.ansible_ssh_common_args // ""')
+ANSIBLE_WEB_PORT=$(echo $JSON |$JQ -r '.ansible_web_port // "8080"' )
+ANSIBLE_WEB_SUBDIR=$(echo $JSON |$JQ -r '.ansible_web_subdir // ""' )
+
+
+echo $ANSIBLE_HOST
+echo $ANSIBLE_PORT
+echo $ANSIBLE_USER
+echo $ANSIBLE_ARGS
+echo $ANSIBLE_WEB_PORT
+echo $ANSIBLE_WEB_SUBDIR
+
+echo starting webbrowser with address https://127.0.0.1:1$ANSIBLE_WEB_PORT$ANSIBLE_WEB_SUBDIR
+echo remote port: $ANSIBLE_WEB_PORT, locale port: 1$ANSIBLE_WEB_PORT
+python -mwebbrowser https://127.0.0.1:1$ANSIBLE_WEB_PORT$ANSIBLE_WEB_SUBDIR &
+
+echo connecting to host...
+eval ssh -p $ANSIBLE_PORT -L 1$ANSIBLE_WEB_PORT:$ANSIBLE_HOST:$ANSIBLE_WEB_PORT root@babelfish.spdns.de
