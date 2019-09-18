adf610b0f0d1f015af4a8889f460703dc291e424
Martin Weil <gthb@postia.de>
2019-09-18 23:23:55 +0200

    Completly overhauled firefox extension update script

--- roles/firefox-client/templates/firefox-extension-update.sh
+++ roles/firefox-client/templates/firefox-extension-update.sh
@@ -4,14 +4,22 @@
 # Datei wird über Ansible bereitgestellt und muss auch dort bearbeitet werden.
 #
 
-if cd /usr/local/share/ansible_downloads; then
-downloads=({% for extension in firefox_extensions %}
-"`wget -N {{ extension.url }}`"{% endfor %}); fi
+PROGNAME=$(basename $0)
+error_str=""
+exit_var=0
+error_exit()
+{
+	error_str="$error_str\n${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit_var=1
+}
 
-for download in "${downloads[@]}"; do
-  if [ -z "$download " ]
-  then echo Something failed.
-       all_succeeded=0
-       break
-  fi
-done
+mkdir -p /usr/local/share/ansible_downloads
+cd /usr/local/share/ansible_downloads
+{% for extension in firefox_extensions %}
+wget -N {{ extension.url }} || error_exit "$LINENO: {{ extension.name }} could not be downloaded."
+{% endfor %}
+
+if [ $exit_var != 0 ]; then
+	echo $error_str | mailx -r "{{ ansible_fqdn }}" -s "Firefox Extension Update fehlgeschlagen: {{ ansible_fqdn }}"  ak-computer@lists.hamburg.de
+	exit 1
+fi
