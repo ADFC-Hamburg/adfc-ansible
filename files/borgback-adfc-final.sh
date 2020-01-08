89683a806365f3747a72b787adead4c475f0ac0a
Volker Kraeft <post@volkerkraeft.de>
2020-01-08 20:19:31 +0100

    1. Erweiterung BorgBackup

--- files/borgback-adfc-final.sh
+++ files/borgback-adfc-final.sh
@@ -4,76 +4,75 @@
 
 # ---------- Variablen-Definition ---------------------------------------------------------
 
-TIMESTAMP=$(date +"%Y%m%d-%H%M%S")                   # Zeitstempel für Logdatei und Backuparchiv
-LOG_DIR="/pfad/zum/logverz"                          # Verzeichnis für Logdaten
-LOG_DATA="borgbackup_${TIMESTAMP}.log"               # Logdatei für aktuelle Sicherung
-LOGFILE="$LOG_DIR/$LOG_DATA"                         # Zur besserern Darstellung im Code ... / FIX!
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")                      # Zeitstempel für Logdatei und Backuparchiv
+LOCK_FILE="/tmp/borgback.lock"
+LOG_DIR="/pfad/zum/logverz"                             # Verzeichnis für Logdaten
+LOG_DATA="borgbackup_${TIMESTAMP}.log"                  # Logdatei für aktuelle Sicherung
+LOGFILE="$LOG_DIR/$LOG_DATA"                            # Zur besseren Darstellung im Code ... / FIX!
 
-CL_ONLINE_DIR="/pfad/zum/verz/bak-clients-online"    # Hier legen die Backup-Satelliten ihre Anmelde-Info ab
+CLIENTS_ONLINE_DIR="/pfad/zum/verz/bak-clients-online"  # Hier legen die Backup-Satelliten ihre Anmelde-Info ab
 
-SECRET="borgbackup-passwort"                         # Kennwort für RepoKey
-SOURCE_DIR="/pfad/zu/den/daten /datenverz"           # Zu archivierende Verzeichnisse (mit Leerzeichen getrennt)
-SOURCE_EXCL="*.tmp"                                  # Sicherungs-Ausnahmen (mit Leerzeichen getrennt)
-REPO_PATH="~/pfad/zum/repository"                    # Pfad zum Backup-Repository auf Backup-Client
-REPO_NAME="name-des-borg-repositories"               # Name des Backup-Repositories auf Backup-Client
-ARCHIVE_NAME="name-des-archivs_${TIMESTAMP}"         # Name der aktuellen Sicherung
-BORG_USER="borguser"                                 # Benutzerkonto auf Backup-Client / FIX! NICHT ÄNDERN
+BAK_SECRET="borgbackup-passwort"                        # Kennwort für RepoKey
+SOURCE_DIR="/pfad/zu/den/daten /datenverz"              # Zu archivierende Verzeichnisse (mit Leerzeichen getrennt)
+SOURCE_EXCLUDE="*.tmp"                                  # Sicherungs-Ausnahmen (mit Leerzeichen getrennt)
+REPO_PATH="~/pfad/zum/repository"                       # Pfad zum Backup-Repository auf Backup-Client
+REPO_NAME="name-des-borg-repositories"                  # Name des Backup-Repositories auf Backup-Client
+ARCHIVE_NAME="name-des-archivs_${TIMESTAMP}"            # Name der aktuellen Sicherung
+BORG_USER="borguser"                                    # Benutzerkonto auf Backup-Client / FIX! NICHT ÄNDERN
 
-PRUNE_D="7"                                          # Anzahl der vorzuhaltenden Tagessicherungen
-PRUNE_W="4"                                          # Anzahl der vorzuhaltenden Wochensicherungen
-PRUNE_M="12"                                         # Anzahl der vorzuhaltenden Monatssicherungen
-PRUNE_Y="2"                                          # Anzahl der vorzuhaltenden Jahressicherungen
+PRUNE_DAYS="7"                                          # Anzahl der vorzuhaltenden Tagessicherungen
+PRUNE_WEEKS="4"                                         # Anzahl der vorzuhaltenden Wochensicherungen
+PRUNE_MONTHS="12"                                       # Anzahl der vorzuhaltenden Monatssicherungen
+PRUNE_YEARS="2"                                         # Anzahl der vorzuhaltenden Jahressicherungen
 
-M_SND="absender@e-mail.tld"                          # Absender
-M_RCV="empfaenger@e-mail.tld"                        # Empfänger
-M_PWD="smtp-passwort"                                # SMTP-Passwort
-M_SVR="mailserver.hoster.tld"                        # SMTP-Server
-M_SUB="Backup-Report"                                # Betreffzeile / sollte so bleiben
-M_BDY="Freier Text für E-Mail-Body."                 # Body Text
-M_ATT="anhang1.ext /pfad/zum/anhang2.ext"            # weitere Anhänge hier eintragen (Backup-Logdatei geht standardmäßig immer mit raus)
-
+MAIL_SENDER="absender@e-mail.tld"                       # Absender
+MAIL_RECEIVER="empfaenger@e-mail.tld"                   # Empfänger
+MAIL_PASSWD="smtp-passwort"                             # SMTP-Passwort
+MAIL_SERVER="mailserver.hoster.tld"                     # SMTP-Server
+MAIL_SUBJECT="ADFC Backup-Report"                       # Betreffzeile / sollte so bleiben
+MAIL_BODY="Freier Text für E-Mail-Body."                # Body Text
+MAIL_ATTACHM="anhang1.ext /pfad/zum/anhang2.ext"        # weitere Anhänge hier eintragen (Backup-Logdatei geht standardmäßig immer mit raus)
 
 
 ###########################################################################################
 ############ AB HIER KEINE ANPASSUNGEN MEHR ERFORDERLICH! #################################
 ###########################################################################################
 
+# Per Lock-Datei verhindern, dass das Programm doppelt gestartet wird
+exec 200>${LOCK_FILE} ||  exit 3
+flock -n 200
 
 # ---------- Einlesen der verbundenen Backup-Clients / Ermitteln der IP -------------------
 
 # Ist kein Client verbunden wird auch nichts unternommen
-if [ "$(ls -A $CL_ONLINE_DIR)" ]; then
-   MACHE="WEITER"
-else
+if [ "$(ls -A $CLIENTS_ONLINE_DIR)" == "" ]; then
    exit 1
 fi
 
 # Einlesen aller Verbindungen
 ALLCLIENTS=()
-for FILES in $CL_ONLINE_DIR/*; do
-    ALLCLIENTS[${#ALLCLIENTS[@]}]="$FILES"
-done
+for CLIENT in $CLIENTS_ONLINE_DIR/*; do
+    ALLCLIENTS[${#ALLCLIENTS[@]}]="$CLIENT"
 
-# Alle in CL_ONLINE_DIR vorhandenen Verbindungen testen, wenn Offline löschen. Nur 192er-Netz zulässig
-for FILES in "${ALLCLIENTS[@]}"; do
-    TEST_IP=$(echo "$FILES" | cut -d '=' -f 2)
-    if [ $(echo "$TEST_IP" | cut -d '.' -f 1) -ne 192 ]; then
-       rm $FILES
-    fi
-    ping -c 3 $TEST_IP 2>&1> /dev/null
-    RESULT=$?
-    if [ $RESULT == 0 ]; then
-       IPADDR=$TEST_IP
-       ONLINE_FILE=$FILES
-    else
-       rm $FILES
-    fi
+    # Alle in CLIENTS_ONLINE_DIR vorhandenen Verbindungen testen, wenn Offline löschen. Nur 192er-Netz zulässig
+    for CLIENT in "${ALLCLIENTS[@]}"; do
+        TEST_IP=$(echo "$CLIENT" | cut -d '=' -f 2)
+        if [ $(echo "$TEST_IP" | cut -d '.' -f 1) -ne 192 ]; then
+           rm $CLIENT
+        fi
+        ping -c 3 $TEST_IP 2>&1> /dev/null
+        RESULT=$?
+        if [ $RESULT == 0 ]; then
+           IPADDR=$TEST_IP
+           ONLINE_CLIENT=$CLIENT
+        else
+           rm $CLIENT
+        fi
+    done
 done
 
 # Falls keine Verbindung funktionierte ist das Verzeinis leer. Dann Abbruch!
-if [ "$(ls -A $CL_ONLINE_DIR)" ]; then
-   MACHE="WEITER"
-else
+if [ "$(ls -A $CLIENTS_ONLINE_DIR)" == "" ]; then
    exit 2
 fi
 
@@ -82,8 +81,8 @@ fi
 # ---------- Backup-Routine ----------------------------------------------------------------
 
 # Repo-Init legt beim erstmaligen Aufruf ein Repository an. Weitere Aufrufe beenden sich ohne Funktion oder Fehler (Rückgabewert=2 spielt hier keine Rolle).
-BORG_PASSPHRASE="$SECRET" borg init --encryption=repokey --show-rc $BORG_USER@$IPADDR:$REPO_PATH/$REPO_NAME 2>&1
-BORG_PASSPHRASE="$SECRET" borg create -v -s -p -C lz4 --show-version --list --show-rc $BORG_USER@$IPADDR:$REPO_PATH/$REPO_NAME::$ARCHIVE_NAME $SOURCE_DIR --exclude $SOURCE_EXCL $LOGFILE 2>&1
+BORG_PASSPHRASE="$BAK_SECRET" borg init --encryption=repokey --show-rc $BORG_USER@$IPADDR:$REPO_PATH/$REPO_NAME 2>&1
+BORG_PASSPHRASE="$BAK_SECRET" borg create -v -s -p -C lz4 --show-version --list --show-rc $BORG_USER@$IPADDR:$REPO_PATH/$REPO_NAME::$ARCHIVE_NAME $SOURCE_DIR --exclude $SOURCE_EXCLUDE $LOGFILE 2>&1
 
 
 
@@ -95,13 +94,13 @@ ssh $BORG_USER@$IPADDR -t "rm ~/BACKUP_*" 2>&1
 # E-Mail bei Erfolg oder Fehlschlag versenden
 if [ $? == 0 ]; then
    ssh $BORG_USER@$IPADDR -t "touch ~/BACKUP_ERFOLGREICH" # 2>&1
-   sendemail -f $M_SND -s $M_SVR -xu $M_SND -xp $M_PWD -t $M_RCV -u "$M_SUB - Datensicherung O.K." -m $M_BDY -a $LOGFILE $M_ATT 2>&1
-   echo "Dateiname = $ONLINE_FILE"
-   rm $ONLINE_FILE
+   sendemail -f $MAIL_SENDER -s $MAIL_SERVER -xu $MAIL_SENDER -xp $MAIL_PASSWD -t $MAIL_RECEIVER -u "$MAIL_SUBJECT - Datensicherung O.K." -m $MAIL_BODY -a $LOGFILE $MAIL_ATTACHM 2>&1
+   echo "Dateiname = $ONLINE_CLIENT"
+   rm $ONLINE_CLIENT
 else
    ssh $BORG_USER@$IPADDR -t "touch ~/BACKUP_FEHLGESCHLAGEN" 2>&1
-   sendemail -f $M_SND -s $M_SVR -xu $M_SND -xp $M_PWD -t $M_RCV -u "$M_SUB - FEHLER BEI DATENSICHERUNG!!" -m $M_BDY -a $LOGFILE $M_ATT 2>&1
-   mv $ONLINE_FILE ${LOG_DIR}_FAILED
+   sendemail -f $MAIL_SENDER -s $MAIL_SERVER -xu $MAIL_SENDER -xp $MAIL_PASSWD -t $MAIL_RECEIVER -u "$MAIL_SUBJECT - FEHLER BEI DATENSICHERUNG!!" -m $MAIL_BODY -a $LOGFILE $MAIL_ATTACHM 2>&1
+   mv $ONLINE_CLIENT ${LOG_DIR}_FAILED
    exit 9
 fi
 
@@ -109,8 +108,11 @@ fi
 
 # ---------- Alte Backups & Logdaten aufräumen --------------------------------------------------------
 
-BORG_PASSPHRASE="$SECRET" borg prune $BORG_USER@$IPADDR:$REPO_PATH/$REPO_NAME --keep-within=1d --keep-daily=$PRUNE_D --keep-weekly=$PRUNE_W --keep-monthly=$PRUNE_M --keep-yearly=$PRUNE_Y
+BORG_PASSPHRASE="$BAK_SECRET" borg prune $BORG_USER@$IPADDR:$REPO_PATH/$REPO_NAME --keep-within=1d --keep-daily=$PRUNE_DAYS --keep-weekly=$PRUNE_WEEKS --keep-monthly=$PRUNE_MONTHS --keep-yearly=$PRUNE_YEARS
 
 find $LOG_DIR/* -mtime 30 -exec rm {} \;
+
+# Lock-Datei wieder freigeben
+flock -u 200
 
 exit 0
