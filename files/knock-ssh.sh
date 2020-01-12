7e5de31db0159063756bf022db77356b091ac6ab
Sven Anders <sven@anders-hamburg.de>
2020-01-12 17:46:11 +0100

    Correct knocking-key ssh path

--- files/knock-ssh.sh
+++ files/knock-ssh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/ssh -o "ConnectTimeout 5" -T -i .ssh/knocking-key root@192.168.123.32 >/dev/null 2>&1
+/usr/bin/ssh -o "ConnectTimeout 5" -T -i /root/.ssh/knocking-key root@192.168.123.32 >/dev/null 2>&1
