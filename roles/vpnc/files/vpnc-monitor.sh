666b7545f5a71635c67b1fcb2d8db49cb41fb9e1
Sven Anders <sven@anders-hamburg.de>
2020-01-13 15:22:13 +0100

    VPNC Restart: Weiterer Fall ins Monitoring aufgenommen

--- roles/vpnc/files/vpnc-monitor.sh
+++ roles/vpnc/files/vpnc-monitor.sh
@@ -6,7 +6,17 @@ PING_ADDR=$2
 JSON=$(journalctl -u $UNIT -n 1 -o json)
 MESSAGE=$(echo $JSON |jq -r .MESSAGE)
 
-if [ "${MESSAGE}" =  "vpnc: recvfrom: No route to host" ] ; then
-    systemctl restart $UNIT
-    exit 0
-fi
+case "${MESSAGE}" in
+    "vpnc: recvfrom: No route to host")
+        systemctl restart $UNIT
+        exit 0
+    ;;
+    "vpnc: HMAC mismatch in ESP mode")
+        systemctl restart $UNIT
+        exit 0
+        ;;
+    *)
+        exit 0
+        ;;
+
+esac
