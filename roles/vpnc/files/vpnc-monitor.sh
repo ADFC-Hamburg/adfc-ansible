cb8cc35f1c9fb58681a59eadee4faa4b5b337186
Sven Anders <sven@anders-hamburg.de>
2020-01-19 07:12:50 +0100

    Borg: Reconnect bei Pingverlust erst nach 10 Minuten

--- roles/vpnc/files/vpnc-monitor.sh
+++ roles/vpnc/files/vpnc-monitor.sh
@@ -2,6 +2,7 @@
 
 UNIT=$1
 PING_ADDR=$2
+STATUSFILE=/run/vpnc-monitor.down
 
 JSON=$(journalctl -u $UNIT -n 1 -o json)
 MESSAGE=$(echo $JSON |jq -r .MESSAGE)
@@ -12,6 +13,16 @@ function restart_vpn
     systemctl restart $UNIT
 }
 
+function no_ping
+{
+    if [ ! -f $STATUSFILE ] ; then
+        touch $STATUSFILE
+    fi
+    OUT=$(find $STATUSFILE -mmin +10)
+    if [ -n "${OUT}" ] ; then
+        restart_vpn
+    fi
+}
 case "${MESSAGE}" in
     "vpnc: recvfrom: No route to host")
         restart_vpn
@@ -22,8 +33,12 @@ case "${MESSAGE}" in
         exit 0
         ;;
 esac
-ping -c 1 $PING_ADDR 2>&1> /dev/null
+ping -c 2 $PING_ADDR 2>&1> /dev/null
 
 if [ $? -ne 0 ]; then
     restart_vpn
+else
+    if [ -f $STATUSFILE ] ; then
+        rm $STATUSFILE
+    fi
 fi
