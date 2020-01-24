8b1053319d774639c49830424139cfcf39ae769d
Sven Anders <sven@anders-hamburg.de>
2020-01-24 07:23:13 +0100

    Thunderbird sysprefs von ansible

---
+++ roles/thunderbird/files/sysprefs.js
@@ -0,0 +1,13 @@
+// ADFC Einstellung siehe Ansible Github
+
+pref("extensions.update.enabled", true);
+
+ 
+// stops the request to send performance data
+pref("toolkit.telemetry.enabled", false);
+
+// stops the request to send performance data from displaying
+pref("toolkit.telemetry.prompted", 2);
+
+// lockPerf geht hier nicht
+pref("mail.server.default.offline_download", false);
