bd2a32feb63853e267bd325901ec5a0f99a3c3f5
Martin Weil <gthb@postia.de>
2020-12-21 14:42:35 +0100

    Added MAC addresses to host_vars and reworked startup script to support waking clients

--- roles/tasmota-power-script/templates/startup_script.sh
+++ roles/tasmota-power-script/templates/startup_script.sh
@@ -13,22 +13,33 @@ clear
 if [ $computer = {{ hostvars[host]['inventory_hostname_short'] }} ]; then
 	ip_client="{{ hostvars[host]['ansible_host'] }}"
 	ip_tasmota="{{ hostvars[host]['tasmota_wireless_ip'] }}"
+	mac_client="{{ hostvars[host]['ansible_host_macaddress'] }}"
 fi
 {% endif %}
 {% endfor %}
 
 if p=$(ping -c1 -W1 $ip_client | grep -i '0 received'); then
-	echo "Der Client $computer ist nicht eingeschaltet."
-	if a=$(curl -s http://$ip_tasmota/cm?cmnd=power | grep -i 'Power = On'); then
-		echo "Die Steckdose war eingeschaltet. Sie wird nun ausgeschaltet und nach einer Minute wieder eingeschaltet. Der Client $computer sollte dann starten."
-		curl -s "http://$ip_tasmota/cm?cmnd=Power+Off" &> /dev/null
-		sleep 60
+	echo "Der Client $computer reagiert nicht."
+	if a=$(curl -s http://$ip_tasmota/cm?cmnd=power | grep -i '"ON"'); then
+		wakeonlan $mac_client
+		echo "Der client wurde aufgeweckt"
+		sleep 5
+		if b=$(ping -c1 -W1 $ip_client | grep -i '0 received'); then
+			echo "Die Steckdose war eingeschaltet. Sie wird nun ausgeschaltet und nach einer Minute wieder eingeschaltet. Der Client $computer sollte dann starten."
+			curl -s "http://$ip_tasmota/cm?cmnd=Power+Off" &> /dev/null
+			sleep 60
+		else
+			echo "Der client $computer hat geschlafen und wurde erfolgreich aufgeweckt"
+			sleep 5
+			exit
+		fi
+	else	
+		echo "Die Steckdose wird jetzt eingeschaltet. Bitte ein paar Sekunden warten bis der Client hochgefahren ist und dann die Verbindung über guacamole aufbauen."
+		curl -s "http://$ip_tasmota/cm?cmnd=Power+On" &> /dev/null
+		sleep 5
+		echo "Dieses Programm wird jetzt automatisch beendet."
+		sleep 5
 	fi
-	echo "Die Steckdose wird jetzt eingeschaltet. Bitte ein paar Sekunden warten bis der Client hochgefahren ist und dann die Verbindung über guacamole aufbauen."
-	curl -s "http://$ip_tasmota/cm?cmnd=Power+On" &> /dev/null
-	sleep 5
-	echo "Dieses Programm wird jetzt automatisch beendet."
-	sleep 5
 else
 	echo "Der Client $computer läuft bereits. Bitte bauen Sie eine Verbindung über guacamole auf."
 	echo "Dieses Programm wird jetzt automatisch beendet."
