a1460fd4d0600a8f4fc098b6e875a3c26a96c505
Sven Anders <sven@anders-hamburg.de>
2022-03-18 18:22:57 +0100

    Chromium Latest

---
+++ roles/chromium-latest/files/chromium-latest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#  Ansible Managed
+A=$(kdialog --title "Installation" --progressbar "Check for a new version of chromium" )
+qdbus $A showCancelButton false
+qdbus $A Set "" "value" 10
+/usr/local/bin/update-chromium-latest
+qdbus $A Set "" "value" 100
+qdbus $A close
+exec /usr/local/chromium/unzip/chrome-linux/chrome $*
