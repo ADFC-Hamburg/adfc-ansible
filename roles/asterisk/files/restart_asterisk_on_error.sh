dfc9d8be3d61369de1376a4f0bedeec06dbee563
Sven Anders <sven@anders-hamburg.de>
2022-06-09 09:26:56 +0200

    Logging messages

--- roles/asterisk/files/restart_asterisk_on_error.sh
+++ roles/asterisk/files/restart_asterisk_on_error.sh
@@ -2,5 +2,6 @@
 REJECTS=$(/usr/sbin/asterisk -x "pjsip show registrations" |grep Rejected)
 
 if [ -n "${REJECTS}" ] ; then
+    /usr/bin/logger "Astersik pjsip cron restart"
     /bin/systemctl restart asterisk
 fi
