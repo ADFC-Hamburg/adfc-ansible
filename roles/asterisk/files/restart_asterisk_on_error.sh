37df7267a8699136d25e898d18310eabd94946e5
Sven Anders <sven@anders-hamburg.de>
2024-02-10 18:49:10 +0100

    Alte Playbooks (adfc-intern/jitsi-meet) entfernt

--- roles/asterisk/files/restart_asterisk_on_error.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-REJECTS=$(/usr/sbin/asterisk -x "pjsip show registrations" |grep Rejected)
-
-if [ -n "${REJECTS}" ] ; then
-    /usr/bin/logger "Astersik pjsip cron restart"
-    /bin/systemctl restart asterisk
-fi
