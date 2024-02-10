082c9430af658e6e077c75bd01e9087a2e2334f7
Sven Anders <sven@anders-hamburg.de>
2024-02-10 19:21:44 +0100

    Megalinter changes

--- roles/adfc_client/templates/check_ips.sh
+++ roles/adfc_client/templates/check_ips.sh
@@ -7,18 +7,17 @@
 ping_targets="{{ check_ips | join(' ') }}"
 failed_hosts=""
 
-for i in $ping_targets
-do
-   ping -c 1 $i > /dev/null
-   if [ $? -ne 0 ]; then
-      if [ "$failed_hosts" == "" ]; then
-         failed_hosts="$i"
-      else
-         failed_hosts="$failed_hosts, $i"
-      fi
-   fi
+for i in $ping_targets; do
+	ping -c 1 $i >/dev/null
+	if [ $? -ne 0 ]; then
+		if [ "$failed_hosts" == "" ]; then
+			failed_hosts="$i"
+		else
+			failed_hosts="$failed_hosts, $i"
+		fi
+	fi
 done
 
 if [ "$failed_hosts" != "" ]; then
-   echo $failed_hosts| mailx -r "proxmox01" -s "Host nicht erreichbar:"  ak-computer@lists.hamburg.adfc.de
+	echo $failed_hosts | mailx -r "proxmox01" -s "Host nicht erreichbar:" ak-computer@lists.hamburg.adfc.de
 fi
