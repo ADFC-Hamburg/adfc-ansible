13f269267986a32d04a66bfce742857f756ad7d2
Falk Hackenberger <falk@huckley.de>
2024-10-21 19:11:12 +0200

    New: create freifunk exporter service

--- roles/freifunk_exporter/files/freifunk-exporter.py
+++ roles/freifunk_exporter/files/freifunk-exporter.py
@@ -9,6 +9,11 @@ import os
 app = Flask(__name__)
 data_store = {}
 
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+
 def fetch_data():
     global data_store
     while True:
@@ -20,7 +25,7 @@ def fetch_data():
             # Connect to the remote server using an SSH key
             private_key_path = os.path.expanduser('~/.ssh/id_rsa')  # Path to your private key
             key = paramiko.RSAKey.from_private_key_file(private_key_path)
-            client.connect('192.168.250.53', username='root', pkey=key)  # Update with your username
+            client.connect('192.168.123.53', username='root', pkey=key)  # Update with your username
 
             # Execute the command
             stdin, stdout, stderr = client.exec_command('gluon-neighbour-info -s "" -l -d ::1 -p 1001 -t 3 -r statistics')
@@ -50,38 +55,44 @@ def fetch_data():
             print(f"An error occurred: {e}")
 
         # Sleep before the next fetch attempt
-        time.sleep(10)  # Fetch new data every 10 seconds
+        time.sleep(60)  
 
 @app.route('/metrics')
 def metrics():
     # Prepare metrics in Prometheus format
     cpu_data = data_store.get('stat', {}).get('cpu', {})
-    idle_time = cpu_data.get('idle', 0)
-    user_time = cpu_data.get('user', 0)
-    system_time = cpu_data.get('system', 0)
-    iowait_time = cpu_data.get('iowait', 0)
-    # Add any other relevant CPU states if needed
+ 
+    metrics = f"""# HELP freifunk_total_clients Total number of clients
+# TYPE freifunk_total_clients gauge
+freifunk_total_clients {data_store.get('clients', {}).get('wifi', 0)}
 
-    # Calculate total CPU time
-    total_cpu_time = idle_time + user_time + system_time + iowait_time  # Extend if needed
+# HELP freifunk_wlan_current_connections current WLAN connections
+# TYPE freifunk__wlan_current_connections gauge
+freifunk_wlan_current_connections{{wlan="2.4 GHz"}} {data_store.get('clients', {}).get('wifi24', 0)}
+freifunk_wlan_current_connections{{wlan="5 Ghz"}} {data_store.get('clients', {}).get('wifi5', 0)}
 
-    # Calculate CPU idle percentage
-    cpu_idle_percentage = (idle_time / total_cpu_time) * 100 if total_cpu_time > 0 else 0
+# HELP traffic_rx_bytes Total received bytes
+# TYPE traffic_rx_bytes counter
+freifunk_traffic_rx_bytes {data_store.get('traffic', {}).get('rx', {}).get('bytes', 0)}
 
-    metrics = f"""
-    # HELP total_clients Total number of clients
-    # TYPE total_clients gauge
-    freifunk_total_clients {data_store.get('clients', {}).get('total', 0)}
+# HELP traffic_tx_packets Total transmited bytes
+# TYPE traffic_tx_packets counter
+freifunk_traffic_tx_bytes {data_store.get('traffic', {}).get('tx', {}).get('bytes', 0)}
 
-    # HELP traffic_rx_packets Total received packets
-    # TYPE traffic_rx_packets counter
-    freifunk_traffic_rx_bytes {data_store.get('traffic', {}).get('rx', {}).get('bytes', 0)}
-    freifunk_traffic_tx_bytes {data_store.get('traffic', {}).get('tx', {}).get('bytes', 0)}
+# HELP freifunk_cpu_seconds_total Seconds the cpus spent in each mode.
+# TYPE freifunk_cpu_seconds_total counter
+freifunk_cpu_seconds_total{{cpu="0",mode="idle"}} {cpu_data.get('idle', 0)}
+freifunk_cpu_seconds_total{{cpu="0",mode="iowait"}} {cpu_data.get('iowait', 0)}
+freifunk_cpu_seconds_total{{cpu="0",mode="irq"}} {cpu_data.get('irq', 0)}
+freifunk_cpu_seconds_total{{cpu="0",mode="nice"}} {cpu_data.get('nice', 0)}
+freifunk_cpu_seconds_total{{cpu="0",mode="softirq"}} {cpu_data.get('softirq', 0)}
+freifunk_cpu_seconds_total{{cpu="0",mode="system"}} {cpu_data.get('system', 0)}
+freifunk_cpu_seconds_total{{cpu="0",mode="user"}} {cpu_data.get('user', 0)}
 
-    # HELP cpu_idle_percentage CPU idle time percentage
-    # TYPE cpu_idle_percentage gauge
-    freifunk_cpu_idle_percentage {cpu_idle_percentage}    
-    """
+# HELP freifunk_node_load load average.
+# TYPE freifunk_node_load gauge
+freifunk_node_load {data_store.get('loadavg', 0)}
+"""
 
     return Response(metrics, mimetype='text/plain')
 
@@ -89,3 +100,4 @@ if __name__ == '__main__':
     # Start the data fetching in a separate thread
     threading.Thread(target=fetch_data, daemon=True).start()
     app.run(port=5000)
+
